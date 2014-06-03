780551e3ccc1b44f526ad25e1c89a871ad870738
fazhar <fazils.azhar@gmail.com>
2014-06-03 12:23:00 -0700

    Basic layout complete. Users controller added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Streams::Application.routes.draw do
     
+  get "users/new"
 	root 'static_pages#home'
     match '/help',    to: 'static_pages#help',    via: 'get'
   	match '/about',   to: 'static_pages#about',   via: 'get'
   	match '/contact', to: 'static_pages#contact', via: 'get'
+  	match '/new_user',to: 'users#new', via: 'get'
 
 end
